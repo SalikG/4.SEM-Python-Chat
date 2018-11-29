3bdb2d339a8617fcef4d16b5b1bed9282e3abab9
Unknown <salikvilmer@gmail.com>
2018-11-29 11:14:27 +0100

    init push

---
+++ Client.py
@@ -0,0 +1,238 @@
+import socket
+import tkinter as tk
+from tkinter import messagebox
+from threading import Thread
+import pickle
+
+
+class Application():
+    def __init__(self, master):
+        self.master = master
+        self.nickname = ""
+        # --------------------GUI_START-----------------------
+
+        master.geometry("300x300")
+        # LOGIN_start
+        self.login_frame = tk.Frame(master)
+
+        username_label = tk.Label(self.login_frame, text="Username")
+        self.username = tk.StringVar()
+        self.username.set("")
+        username_input_field = tk.Entry(self.login_frame, textvariable=self.username)
+        # Binder input field til 'enter' kommando fra keyboard og kalder 'send' metoden
+        username_input_field.bind("<Return>", lambda e: self.send_login())
+
+        password_label = tk.Label(self.login_frame, text="Password")
+        self.password = tk.StringVar()
+        self.password.set("")
+        password_input_field = tk.Entry(self.login_frame, textvariable=self.password)
+        # Binder input field til 'enter' kommando fra keyboard og kalder 'send' metoden
+        password_input_field.bind("<Return>", lambda e: self.send_login())
+
+        btn_login = tk.Button(self.login_frame, text="Login", bg="green", command = self.send_login)
+        btn_register = tk.Button(self.login_frame, text="Register", bg="blue", command=lambda: self.raise_frame(self.register_frame))
+
+        # PACKING_START
+        username_label.pack(side=tk.TOP)
+        username_input_field.pack(side=tk.TOP, fill=tk.X)
+        password_label.pack(side=tk.TOP)
+        password_input_field.pack(side=tk.TOP, fill=tk.X)
+        btn_login.pack(side=tk.BOTTOM)
+        btn_register.pack(side=tk.BOTTOM)
+
+        self.login_frame.grid(row=0, column=0, sticky="NSEW", padx=40, pady=40)
+        # PACKING_END
+        # LOGIN_END
+
+        # REGISTER_START
+        self.register_frame = tk.Frame(master)
+
+        # global register_username
+        register_username_label = tk.Label(self.register_frame, text="Username")
+        self.register_username = tk.StringVar()
+        self.register_username.set("")
+        register_username_input_field = tk.Entry(self.register_frame, textvariable=self.register_username)
+        # Binder input field til 'enter' kommando fra keyboard og kalder 'send' metoden
+        register_username_input_field.bind("<Return>", lambda e: self.send_register())
+
+        # global register_nickname
+        register_nickname_label = tk.Label(self.register_frame, text="Nickname")
+        self.register_nickname = tk.StringVar()
+        self.register_nickname.set("")
+        register_nickname_input_field = tk.Entry(self.register_frame, textvariable=self.register_nickname)
+        # Binder input field til 'enter' kommando fra keyboard og kalder 'send' metoden
+        register_nickname_input_field.bind("<Return>", lambda e: self.send_register())
+
+        # global register_password
+        register_password_label = tk.Label(self.register_frame, text="Password")
+        self.register_password = tk.StringVar()
+        self.register_password.set("")
+        register_password_input_field = tk.Entry(self.register_frame, textvariable=self.register_password)
+        # Binder input field til 'enter' kommando fra keyboard og kalder 'send' metoden
+        register_password_input_field.bind("<Return>", lambda e: self.send_register())
+
+        btn_confirm_register = tk.Button(self.register_frame, text="Register", bg="green", command = self.send_register)
+        btn_back_to_login = tk.Button(self.register_frame, text="Back", command=lambda: self.raise_frame(self.login_frame))
+
+        # PACKING_START
+        register_username_label.pack(side=tk.TOP)
+        register_username_input_field.pack(side=tk.TOP, fill=tk.X)
+        register_nickname_label.pack(side=tk.TOP)
+        register_nickname_input_field.pack(side=tk.TOP, fill=tk.X)
+        register_password_label.pack(side=tk.TOP)
+        register_password_input_field.pack(side=tk.TOP, fill=tk.X)
+        btn_confirm_register.pack(side=tk.BOTTOM)
+        btn_back_to_login.pack(side=tk.BOTTOM)
+
+        self.register_frame.grid(row=0, column=0, sticky="NSEW", padx=40, pady=40)
+        # PACKING_END
+        # REGISTER_END
+
+        self.login_frame.tkraise()
+
+    # bliver kaldt hvis server sender action: login_failed
+    def login_failed(self):
+        login_failed = tk.Label(self.login_frame, text="LOGIN FAILED", bg="red")
+        login_failed.pack(side=tk.TOP)
+
+    def chat_frame(self):
+        msg_frame = tk.Frame(self.master)
+
+        scrollbar = tk.Scrollbar(msg_frame)
+        global msg_list
+        msg_list = tk.Listbox(msg_frame, height=30, width=60, yscrollcommand=scrollbar.set)
+
+        global msg
+        msg = tk.StringVar()
+        msg.set("")
+        msg_input_field = tk.Entry(msg_frame, textvariable=msg)
+        # Binder input field til 'enter' kommando fra keyboard og kalder 'send' metoden
+        msg_input_field.bind("<Return>", lambda e: self.send())
+        btn_send = tk.Button(msg_frame, text="Send", bg="green", command=self.send)
+
+        # PACKING_START
+        btn_send.pack(side=tk.BOTTOM)
+        msg_input_field.pack(side=tk.BOTTOM, fill=tk.X)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        msg_list.pack(side=tk.LEFT, fill=tk.BOTH)
+        msg_list.pack()
+        self.master.geometry("400x540")
+        msg_frame.grid(row=0, column=0, padx=4, pady=4)
+        # PACKING_END
+        msg_frame.tkraise()
+
+    def raise_frame(self, frame=tk.Frame):
+        frame.tkraise()
+
+    def send_login(self):
+        if self.reg_login_input_validation(self.username.get()) and self.reg_login_input_validation(self.password.get()):
+            msg = {"action": "login", "username": self.username.get(), "password": self.password.get()}
+            data = pickle.dumps(msg)
+            mySocket.send(data)
+        else:
+            messagebox.showerror("Error", "There cant be 'Spaces' in the text!")
+
+
+    def send(self):
+        global msg
+        message = {"action":"msg", "msg":msg.get(), "nickname":self.nickname}
+        print("Sending message:  ", message)
+        msg.set("")
+
+        data = pickle.dumps(message)
+        mySocket.send(data)
+
+    def send_register(self):
+        if self.reg_login_input_validation(self.register_username.get()) == True and \
+                self.reg_login_input_validation(self.register_nickname.get()) and \
+                self.reg_login_input_validation(self.register_password.get()) == True:
+            msg = {"action": "reg", "username": self.register_username.get(), "nickname": self.register_nickname.get(),
+                   "password": self.register_password.get()}
+            data = pickle.dumps(msg)
+            mySocket.send(data)
+        else:
+            messagebox.showerror("Error", "There cant be 'Spaces' in the text!")
+
+    def reg_login_input_validation(self, inpt):
+        if ' ' in inpt:
+            return False
+        else:
+            return True
+
+    def error_frame(self, error_msg):
+        self.login_frame.grid_remove()
+        self.register_frame.grid_remove()
+        error_frame = tk.Frame(self.master)
+        error_label = tk.Label(error_frame, text=error_msg, bg="red")
+        error_label.pack(fill=tk.BOTH)
+        error_frame.grid(row=0, column=0, padx=4, pady=4)
+        error_frame.tkraise()
+
+def receive():
+    while True:
+        try:
+            data = mySocket.recv(1024)
+            msg = pickle.loads(data)
+            print("msg from server: ", msg)
+            if msg["action"] == "msg":
+                global msg_list
+                if msg.get('nickname', None) == None:
+                    msg_list.insert(tk.END, msg["msg"])
+                    msg_list.see(tk.END)
+                else:
+                    msg_list.insert(tk.END, msg["nickname"] + ":  " + msg["msg"])
+                    msg_list.see(tk.END)
+            elif msg["action"] == "login_success":
+                print("login was a success")
+                app.username.set("")
+                app.password.set("")
+                app.nickname = msg["nickname"]
+                app.chat_frame()
+            elif msg["action"] == "login_failed":
+                app.login_failed()
+                print("login FAILED")
+            elif msg["action"] == "reg_success":
+                app.register_username.set("")
+                app.register_nickname.set("")
+                app.register_password.set("")
+                app.raise_frame(app.login_frame)
+                print("reg was a success")
+            elif msg["action"] == "server_closing":
+                app.error_frame("Server is down")
+        except OSError:
+            break
+
+
+
+try:
+    host = '127.0.0.1'
+    port = 9000
+
+    mySocket = socket.socket()
+    mySocket.connect((host, port))
+    reciever_thread = Thread(target=receive)
+    reciever_thread.start()
+
+
+
+except Exception:
+    root = tk.Tk()
+
+    app = Application(root)
+    root.title("Hacker Paradise")
+
+    app.error_frame("SERVER ERROR:  server is down or connection is lost")
+
+    root.mainloop()
+else:
+    root = tk.Tk()
+    app = Application(root)
+    root.title("Hacker Paradise")
+
+    root.mainloop()
+
+    mySocket.close()
+
+
+
+
---
+++ Server.py
@@ -0,0 +1,79 @@
+import socket
+import threading
+import queue
+import time
+import pickle
+
+clients = []
+messages = queue.Queue()
+
+host = "127.0.0.1"
+
+port = 9000
+
+connection = socket.socket()
+connection.bind((host, port))
+connection.listen(1)
+
+def acceptConnections():
+    names = 0
+    while True:
+        conn, addr = connection.accept()
+        client_dict = {"NICKNAME": names, "CONNECTION_TS": time.time(), "CLIENT": conn}
+        clients.append(client_dict)
+        names += 1
+        print("New Client Connected: ", addr)
+        t = threading.Thread(target=client_thread, args=(conn,))
+        t.start()
+
+def broadcast_messages():
+    while True:
+        msg = messages.get()
+        for c in clients:
+            data = pickle.dumps(msg)
+
+            c["CLIENT"].send(data)
+
+def client_thread(conn):
+    while True:
+        data = conn.recv(1024)
+        msg = pickle.loads(data)
+        print("New message ", msg)
+
+        if msg["action"] == "reg":
+            file = open("users.txt", "a")
+            file.write(msg["username"] + " " + msg["nickname"] + " " + msg["password"] + "\n")
+            file.close()
+
+            response_msg = {"action":"reg_success"}
+            response_data= pickle.dumps(response_msg)
+            conn.send(response_data)
+        elif msg["action"] == "msg":
+            messages.put(msg)
+        elif msg["action"] == "login":
+            file = open("users.txt", "r")
+            users = file.read().splitlines()
+            login_success = False
+            for line_index in  range(users.__len__()):
+                temp_user = users[line_index].split(" ")
+                if temp_user[0] == msg["username"] and temp_user[2] == msg["password"]:
+                    login_msg = pickle.dumps({"action":"msg", "msg":temp_user[1] + " has joined the room"})
+                    for client_index in range(clients.__len__()):
+                        if clients[client_index]["CLIENT"] == conn:
+                            clients[client_index]["NICKNAME"] = temp_user[1]
+                        else:
+                            # besked til alle clients undtagen den som prøver at logge ind om at en ny client er logget in
+                            clients[client_index]["CLIENT"].send(login_msg)
+                    response_data = pickle.dumps({"action":"login_success", "nickname":temp_user[1]})
+                    conn.send(response_data)
+                    login_success = True
+
+            if login_success == False:
+                response_data = pickle.dumps({"action": "login_failed"})
+                conn.send(response_data)
+
+        print("New message ", msg)
+
+
+threading.Thread(target=broadcast_messages).start()
+acceptConnections()
